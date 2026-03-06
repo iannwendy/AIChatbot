27f164917f81843f40f58efdd04c74498daa67d4
Huu Duc <hohuuduc31102005@gmail.com>
2026-03-06 13:20:36 +0700

    add code in dockerfile and supply testing data in python

--- backend/create_test_data.py
+++ backend/create_test_data.py
@@ -46,3 +46,79 @@ print(f'Course: {course.name}, created: {created}')
 course.students.add(student)
 print(f'Student enrolled in course')
 
+
+# #!/usr/bin/env python
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# import django
+# django.setup()
+
+# from django.contrib.auth import get_user_model
+# from apps.courses.models import Course
+
+# User = get_user_model()
+
+# print("Creating users...")
+
+# # Teachers
+# teachers = []
+# for i in range(1, 4):
+#     teacher, created = User.objects.get_or_create(
+#         email=f'teacher{i}@test.com',
+#         defaults={
+#             'username': f'teacher{i}',
+#             'role': 'teacher',
+#             'first_name': 'Teacher',
+#             'last_name': str(i)
+#         }
+#     )
+
+#     teacher.set_password("123456")
+#     teacher.save()
+
+#     teachers.append(teacher)
+#     print(f'Teacher {teacher.email} created: {created}')
+
+# # Students
+# students = []
+# for i in range(1, 6):
+#     student, created = User.objects.get_or_create(
+#         email=f'student{i}@test.com',
+#         defaults={
+#             'username': f'student{i}',
+#             'role': 'student',
+#             'first_name': 'Student',
+#             'last_name': str(i)
+#         }
+#     )
+
+#     student.set_password("123456")
+#     student.save()
+
+#     students.append(student)
+#     print(f'Student {student.email} created: {created}')
+
+# print("Creating courses...")
+
+# courses = []
+# for i in range(1, 4):
+#     course, created = Course.objects.get_or_create(
+#         name=f'Test Course {i}',
+#         defaults={
+#             'description': f'Description for course {i}',
+#             'teacher': teachers[i % len(teachers)]
+#         }
+#     )
+
+#     courses.append(course)
+#     print(f'Course {course.name} created: {created}')
+
+# print("Enrolling students...")
+
+# for course in courses:
+#     for student in students:
+#         course.students.add(student)
+
+# print("Data creation completed!")
+
